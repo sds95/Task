b304bf4b18ccfc6ec0572dd587d72a54b3955664
SDS <SDS@SDS>
2021-04-22 23:14:01 +0300

    tasks

---
+++ Napominalka/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Napominalka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            Task[] tasks = new Task[50000];
+            int i = 1;
+            while (!exit)
+            {
+                Console.WriteLine("Введите команду: ");
+                string command = Console.ReadLine()
+                    .ToLowerInvariant();
+
+                switch (command)
+                {
+                    case "create":
+                        Guid guid = Guid.NewGuid();
+                        Console.WriteLine("Введите текст заметки: ");
+                        string data = Console.ReadLine();
+                        Task task = new Task(guid, data);
+                        tasks[i] =  task;
+                        i++;
+                        break;
+                    case "list -all":
+                        Console.WriteLine("Ваши заметки: ");
+                        for (int j = 1; j < i; j++)
+                        {
+                            Console.WriteLine(j + " " + tasks[j]);
+                        }
+                        break;
+                    case "find":
+                        string find = Console.ReadLine();
+                        switch (find)
+                        {
+                            case "-st":
+                                break;
+                            case "-end":
+                                break;
+                            case "-cont":
+                                break;
+                            case "-ind":
+                                break;
+                        }
+                        break;                          
+                    case "del":
+                        Console.WriteLine("Введите номер заметки, которую хотите удалить: ");
+                        int x = Convert.ToInt32(Console.ReadLine());
+                        tasks[x] = null;
+                        break;
+                    case "help":
+
+                    case "exit":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Error command");
+                        break;
+                }         
+            }
+        }
+    }
+}
---
+++ Napominalka/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Napominalka
+{
+    class Task
+    {
+        private Guid id;
+        private string data;
+
+        public override string ToString()
+        {
+            return data;
+        }
+
+        public Task( Guid id, string data)
+        {
+            this.id = id;
+            this.data = data;
+        }
+    }
+}
